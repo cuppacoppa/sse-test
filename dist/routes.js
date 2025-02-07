3747f06f64a8cdaca101d4d45289fe3b5bed9468
cuppacoppa <89590703+cuppacoppa@users.noreply.github.com>
2025-02-07 12:34:21 -0500

    added individualPartnerPage data shape

--- dist/routes.js
+++ dist/routes.js
@@ -496,6 +496,22 @@
     constructor() {
     }
     setup() {
+      const partnersMainPage = [
+        {
+          name: "Partner A",
+          logoUrl: "https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png",
+          Header: "Page Header",
+          subHeader: "Page Sub Header",
+          description: "description",
+          largeMedia: "photo or video link here",
+          caseStudySubHeading: "caseStudySubHeading",
+          caseStudyProblem: "caseStudyProblem",
+          caseStudySolution: "caseStudySolution",
+          caseStudyCollaboration: "caseStudyCollaboration",
+          caseStudyMedia: "case study photo or video link here"
+        },
+        { name: "Partner B", logoUrl: "https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png" }
+      ];
     }
     exec() {
       console.log("individual partner page");
