e2fdccd79a4c6fcfd38c7bf5dfac3b0f8855ad2b
cuppacoppa <89590703+cuppacoppa@users.noreply.github.com>
2025-02-10 12:13:49 -0500

    cleaned up comments and old code

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,6 @@ import { routeDispatcher } from "./routes";
 import { initSSE } from "@sygnal/sse"; 
 import { ComponentManager } from "./engine/component-manager";
 import { TestComponent } from "./components/test";
-// import { mainPartnerCard } from "./components/mainPartnerCard";
-// import { applicationPartnerCard } from "./components/applicationPartnerCard";
 
 interface SiteGlobalDataType {
     // Define properties and their types for SiteDataType
@@ -90,32 +88,6 @@ const exec = () => {
                 case 'test':
                     (new TestComponent(element)).exec();
                     break;
-    
-                // case 'main-partner-card-list': 
-                //     const mainPartnerSection = element;  // Get the section where main partners will be rendered
-                //     const partnersMain = [
-                //         { name: 'Partner A', logoUrl: 'https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png', link: 'https://partnerA.com' },
-                //         { name: 'Partner B', logoUrl: 'https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png', link: 'https://partnerB.com' },
-                //     ];
-                //     mainPartnerSection.classList.add("partner-card-list"); // Add flex container class
-                //     partnersMain.forEach((partner) => {
-                //         // Assuming mainPartnerCard returns HTML for each partner, add it to the container
-                //         mainPartnerSection.innerHTML += mainPartnerCard(partner);  
-                //     });
-                //     break;
-    
-                //     case 'application-partner-card-list': 
-                //     const appPartnerSection = element;  // Get the section where app partners will be rendered
-                //     const partnersApp = [
-                //         { logoUrl: 'https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png' },
-                //         { logoUrl: 'https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png' },
-                //     ];
-                //     appPartnerSection.classList.add("application-partner-card-list"); // Add flex container class
-                //     partnersApp.forEach((partner) => {
-                //         // Assuming applicationPartnerCard returns HTML for each partner, add it to the container
-                //         appPartnerSection.innerHTML += applicationPartnerCard(partner);
-                //     });
-                //     break;
     
                 default:
                     console.log('Unknown component:', componentValue);
