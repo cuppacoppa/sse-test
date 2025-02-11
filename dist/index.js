8a7bcf19123cd78d3442c6dbbcdcbe2c11a0de71
cuppacoppa <89590703+cuppacoppa@users.noreply.github.com>
2025-02-11 13:00:15 -0500

    updated styling, added partners

--- dist/index.js
+++ dist/index.js
@@ -442,9 +442,15 @@
 
   // src/pages/partner.ts
   var partnersApp = [
-    { logoUrl: "https://cdn.prod.website-files.com/679e860587d809fd4d2d7b55/67aa34cea35c6f2a2eda70bb_Oracle_Logo.svg.png", tags: ["Productivity"] },
-    { logoUrl: "https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png", tags: ["Communication"] },
-    { logoUrl: "https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png", tags: ["Productivity", "Security"] }
+    { logoUrl: "https://cdn.prod.website-files.com/679e860587d809fd4d2d7b55/67aa34cea35c6f2a2eda70bb_Oracle_Logo.svg.png", tags: ["Cloud"] },
+    { logoUrl: "https://cdn.prod.website-files.com/660bffb9ffdd26c353bd02d3/67ab88da5ae37db0d0e5b30b_SparkCognition_Logo.jpg", tags: ["Force Protection"] },
+    { logoUrl: "https://cdn.prod.website-files.com/660bffb9ffdd26c353bd02d3/67ab88d95d4e17f85b833e51_instant%20connect%201.png", tags: ["Bridging"] },
+    { logoUrl: "https://cdn.prod.website-files.com/660bffb9ffdd26c353bd02d3/672e773f1a215de4e3b140a8_Corero_Network_Security_Logo.jpg", tags: ["Security"] },
+    { logoUrl: "https://cdn.prod.website-files.com/660bffb9ffdd26c353bd02d3/67ab88d99407a3a9399e97ce_BoonLogic_Main-Logo.png", tags: ["Artificial Intelligence"] },
+    { logoUrl: "https://cdn.prod.website-files.com/660bffb9ffdd26c353bd02d3/67ab88d87da80c137d3dbd0f_Tiami_logo-4-1-1.png", tags: ["C-UAS"] },
+    { logoUrl: "https://cdn.prod.website-files.com/660bffb9ffdd26c353bd02d3/67ab88d8f852614075082571_R2_footer-logo-1024x848.png", tags: ["C-UAS"] },
+    { logoUrl: "https://cdn.prod.website-files.com/660bffb9ffdd26c353bd02d3/67ab88d8e7151d4b8cd03b29_element.png", tags: ["Productivity"] },
+    { logoUrl: "https://cdn.prod.website-files.com/660bffb9ffdd26c353bd02d3/67ab88d87da80c137d3dbd1c_Amazon_Web_Services_Logo.svg.png", tags: ["Cloud"] }
   ];
   var PartnerPage = class {
     constructor() {
